976ea765b637fa5983115566b50ed44b895a0c1f
anya.shan <anya.shanaida@yahoo.com>
2022-11-10 13:02:44 +0200

    change nav js

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,19 +1,13 @@
 import { AppWrap, NavWrap, NavItem } from './AppBar.styled';
 
-const navItems = [
-  { href: 'home', text: 'Home' },
-  { href: 'movies', text: 'Movies' },
-];
-
 export const AppBar = () => {
   return (
     <AppWrap>
       <NavWrap>
-        {navItems.map(({ href, text }) => (
-          <NavItem to={href} key={href}>
-            {text}
-          </NavItem>
-        ))}
+        <NavItem to="/" end>
+          Home
+        </NavItem>
+        <NavItem to="/movies">Movies</NavItem>
       </NavWrap>
     </AppWrap>
   );
